a115144d0d2026b3ef7129ae80b0457658313a5f
kyousuke916 <kyousuke916@gmail.com>
2020-12-09 23:27:07 +0800

    first commit

---
+++ Runtime/DvLog.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.kyousuke916.test
+{
+    public class DvLog : MonoBehaviour
+    {
+        void Awake()
+        {
+            Log("Hello v1.0.0");
+        }
+
+        public void Log(string msg)
+        {
+            Debug.Log($"[DvLog]{msg}");
+        }
+    }
+}
